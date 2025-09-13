491ffbaf899c3fd3ac8e0e55d0f1fdf62beb85e4
AlyadotSalsabila <alya_salsabila_33rpl@student.smktelkom-mlg.sch.id>
2025-09-13 18:53:22 +0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex items-center p-10 h-screen">
+        <h1 className="font-bold text-8xl text-center m-auto">Welcome</h1>
+      </div>
+    </div>
+  );
+}
